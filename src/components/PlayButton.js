1e0f300c11bd2551a980f2a10a5c600ff3fae4bb
Saaheel Majethia <saaheel.majethia@infogain.com>
2020-10-05 01:35:24 +0530

    Added array for static scores

--- src/components/PlayButton.js
+++ src/components/PlayButton.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
-
+const staticScore = {
+    0:'out',
+    1:1,
+    2:2,
+    3:1,
+    4:4,
+    5:0,
+    6:6,
+    7:0,
+    8:1,
+    9:0,
+}
 
 class PlayButton extends Component{
 
@@ -10,52 +21,74 @@ class PlayButton extends Component{
         this.showWinner = this.showWinner.bind(this);
         this.state={
             'player1':0,
+            'player1Disbled':true,
             'player2':0,
-            'winner':''
+            'player2Disbled':true,
+            'winner':'__________',
+            'note':'__________',
         }
     }
 
     generateRandomRuns(){
 
-        return Math.floor(Math.random() * 10);
+        const randNumber = Math.floor(Math.random() * 10);
+        console.log(randNumber);
+        return staticScore[randNumber];
     }
     playEachBall(player){
 
         const newRun = this.generateRandomRuns();
+        if(newRun !== 'out'){
+            if(player === 1){
+                this.setState({
+                    player1:this.state.player1+newRun,
+                });
+            }
 
-        if(player === 1){
-            this.setState({
-                player1:this.state.player1+newRun,
-            });
-        }
+            if(player === 2){
+                this.setState({
+                    player2:this.state.player2+newRun,
+                });
+            }
+        }else{
+            if(player === 1){
+                this.setState({
+                    player1Disbled:false,
+                    note:'player 1 got out'
+                });
+            }
 
-        if(player === 2){
-            this.setState({
-                player2:this.state.player2+newRun,
-            });
+            if(player === 2){
+                this.setState({
+                    player2Disbled:false,
+                    note:'player 2 got out'
+                });
+            }
+            
         }
         
     }
 
     showWinner(){
+
         if(this.state.player1 > this.state.player2){
-            this.setState({winner:'player 1'})
+            this.setState({note:'player 1 won the match'})
         }
 
         if(this.state.player1 < this.state.player2){
-            this.setState({winner:'player 2'})
+            this.setState({note:'player 2 won the match'})
         }
     }
 
     render(){
         return(
             <>
-            <button onClick={() => this.playEachBall(1)}>Player 1 Play</button>
-            <button onClick={() => this.playEachBall(2)}>Player 2 Play</button>
+            <button disabled={!this.state.player1Disbled} onClick={() => this.playEachBall(1)}>Player 1 Play</button>
+            <button disabled={!this.state.player2Disbled} onClick={() => this.playEachBall(2)}>Player 2 Play</button>
             <p>Player 1 Score {this.state.player1}</p>
             <p>Player 2 Score {this.state.player2}</p>
             <p><button onClick={this.showWinner}>Show Result</button></p>
-            <p>winner is {this.state.winner}</p>
+            <p>{this.state.note}</p>
             </>
         );    
     }
